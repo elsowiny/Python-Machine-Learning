2457e45cafded95b169de57b7f58ec5d862ff39b
elsowiny <elsowiny@yahoo.com>
2020-10-13 22:01:04 -0400

    added and defined testing data variables and label and features and test size for the model

--- tester.py
+++ tester.py
@@ -8,4 +8,14 @@ data = pd.read_csv("student-mat.csv", sep=";")
 print(data.head())
 
 # trimming the data
-data = data[["G1", "G2", "G3", "studytime","failures","absences"]]
+data = data[["G1", "G2", "G3", "studytime","failures","absences"]] #our attributes
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1)) #features
+# we drop our attribute that we want to predict from the data array and in turn
+# the rest of our attributes are then, #Features
+#feature is an input variable—the x variable in simple linear regression
+y = np.array(data[predict]) #label(y), the thing we are trying to predict in our linear regression
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
