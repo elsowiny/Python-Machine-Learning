2f1e6b76b70d225314285447f2634c7f58780cbf
Sherief Elsowiny <elsowiny@yahoo.com>
2021-01-18 21:50:44 -0500

    Added the code - to add more explanation later

--- linearRegression/README.md
+++ linearRegression/README.md
@@ -1,2 +1,12 @@
 #Linear Regression
-### [Python Machine Learning Repo](https://github.com/elsowiny/Python-Machine-Learning)
+#### [Python Machine Learning Repo](https://github.com/elsowiny/Python-Machine-Learning)
+
+###Objective: To predict student's final grades based on a student performance dataset
+This project utilises well known libraries available in the Python language for data manipulation, and data science. In this project, I explore using these tools and explore machine learning in Python. The project consists of downloading a large data set of student performance. Using Python, and specific libraries such as pandas and numpy, we are able to perform linear regression on our data in order to predict students final grades based on a series of attributes.
+
+###The Data: UCI Machine Learning Repository
+(Student data that will be used to predict grades, included as a csv file)
+
+##Linear Regression
+Our first approach at predicting student's final grades will consist of our program implementing the linear regression algorithim to which it will generate a best fit line through the data points. The line is defined as y = mx + b (a familiar equation)
+Once the program finds this best fit line, we will use it to predict certain values, in this case final grades.
--- linearRegression/tester.py
+++ linearRegression/tester.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import sklearn
 from sklearn import linear_model
+import sklearn
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -9,10 +9,12 @@ import pickle
 
 
 """
-In this project, we will be exploring linear regression, which is essentially a best fit line that is dependent on our input.
+In this project, we will be exploring linear regression.
+Linear Regression is essentially a best fit line that is dependent on our input.
 We will be using linear regression on our data set to predict the student's final grades.
 Linear regression is best used when the data is correlated. 
-In our case we will be observing data from the student's past grades, their studytime, their failures, and their absences.
+In our case we will be observing data from the student's past grades. 
+Such as: their study time, their failures, and their absences.
 """
 
 data = pd.read_csv("student-mat.csv", sep=";")
@@ -23,19 +25,38 @@ data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]  # our attr
 
 predict = "G3"
 
-X = np.array(data.drop([predict], 1))  # features
+x = np.array(data.drop([predict], 1))  # features
 # we drop our attribute that we want to predict from the data array and in turn
 # the rest of our attributes are then, #Features
 # feature is an input variable—the x variable in simple linear regression
 y = np.array(data[predict])  # label(y), the thing we are trying to predict in our linear regression
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()  #Our model
+# Now we will train our model x number of times, to get our best prediction
+# We can change the var numberOfTrainings to decide how many iterations to try for
+# An acc of 85% is good, an acc of 95% is even better
+"""
+best = 0
+numberOfTrainings = 20
+for _ in range(numberOfTrainings):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+    linear = linear_model.LinearRegression()  # We create a new model named linear
+    linear.fit(x_train, y_train)  # Finding the best fit
+    acc = linear.score(x_test, y_test)  # our accuracy
+    print("This model has an accuracy of: ", acc)
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print("Our model can predict with accuracy of :", acc)
+    if acc > best:
+        best = acc  # Change our target accuracy score for each subsequent model to achieve
+        with open("finalGradePredictModel.pickle", "wb") as f:
+            pickle.dump(linear, f)  # We save the best model until a better one comes along (while we're in the loop)
+"""
+
+
+# Now we must load our model
+
+pickle_in = open("finalGradePredictModel.pickle", "rb")  # We open our pickle file containing our best model
+linear = pickle.load(pickle_in)  # Now we load up our model and get ready to use it
 
 print('Coefficient: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
@@ -44,4 +65,12 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print("computer predicted a grade of ", predictions[x],"using the input values of ", x_test[x],
-          "The actual grade is:", y_test[x]) #printing our predictions, our input values, and our actual value
+          "The actual grade is:", y_test[x])  # Printing our predictions, our input values, and our actual value
+
+
+plot = "G1"  # One of our attributes
+plt.scatter(data[plot], data["G3"])
+plt.legend(loc=4)
+plt.xlabel(plot)
+plt.ylabel("Final Grade")
+plt.show()
